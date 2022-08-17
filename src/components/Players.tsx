bd6bcc43aa18674fd30001ae9a8cd34dc8e908b5
Craig <indigomx9@gmail.com>
2022-08-17 01:33:53 -0400

    Updated Players and Main components.

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../global/Hooks";
 import { API } from "../global/FetchAPI"
 import { Item } from "./Item";
+import { toast } from "react-toastify";
 
 export const Players = (): JSX.Element => {
     const dispatch = useAppDispatch();
     const { error, loading, players } = 
         useAppSelector((state) => state.players);
+
+    React.useEffect(() => {
+        if (error) toast.error("Something went wrong");
+    }, [error]);
 
     React.useEffect(() => {
         dispatch(API.fetchAll());
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Players } from "../components/Players";
 import { Add } from "../containers/Add";
@@ -7,6 +9,7 @@ import { Edit } from "../containers/Edit";
 export const Main = (): JSX.Element => (
     <BrowserRouter>
         <React.Fragment>
+            <ToastContainer />
             <Routes>
                 <Route path="*" element={<Navigate to="/players" />} />
                 <Route path="/players" element={<Players />} />
