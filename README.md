1be9b9bab43cd4b1ef8294ad6239ddaec92aa861
Craig <indigomx9@gmail.com>
2022-08-16 21:21:30 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# PERN
+
+Initial setup of the project.  Created with TypeScript, Webpack, React, React Router, Fetch API, and Redux-Toolkit.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi illo, recusandae omnis, fugit officia aliquid sapiente quae ipsum unde, nostrum esse optio magni eos tenetur eveniet est quaerat culpa dolores!</p>
+        </React.Fragment>
+    );
+};
+
+
+
